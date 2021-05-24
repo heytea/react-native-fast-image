afaef414501b5f09790eb6d9119f37f4f8694f70
qindachang <823951895@qq.com>
2021-05-24 15:23:51 +0800

    fix: transparent image when using react-native-screen in back

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewManager.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewManager.java
@@ -126,22 +126,26 @@ class FastImageViewManager extends SimpleViewManager<FastImageViewWithUrl> imple
     }
 
     @Override
-    public void onDropViewInstance(FastImageViewWithUrl view) {
+    public void onDropViewInstance(final FastImageViewWithUrl view) {
         // This will cancel existing requests.
-        if (requestManager != null) {
-            requestManager.clear(view);
-        }
+        view.setOnDetachedFromWindowListener(new FastImageViewWithUrl.OnDetachedFromWindowListener() {
+            @Override
+            public void onDetached() {
+                if (requestManager != null) {
+                    requestManager.clear(view);
+                }
 
-        if (view.glideUrl != null) {
-            final String key = view.glideUrl.toString();
-            FastImageOkHttpProgressGlideModule.forget(key);
-            List<FastImageViewWithUrl> viewsForKey = VIEWS_FOR_URLS.get(key);
-            if (viewsForKey != null) {
-                viewsForKey.remove(view);
-                if (viewsForKey.size() == 0) VIEWS_FOR_URLS.remove(key);
+                if (view.glideUrl != null) {
+                    final String key = view.glideUrl.toString();
+                    FastImageOkHttpProgressGlideModule.forget(key);
+                    List<FastImageViewWithUrl> viewsForKey = VIEWS_FOR_URLS.get(key);
+                    if (viewsForKey != null) {
+                        viewsForKey.remove(view);
+                        if (viewsForKey.size() == 0) VIEWS_FOR_URLS.remove(key);
+                    }
+                }
             }
-        }
-
+        });
         super.onDropViewInstance(view);
     }
 
--- android/src/main/java/com/dylanvann/fastimage/FastImageViewWithUrl.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewWithUrl.java
@@ -7,8 +7,25 @@ import com.bumptech.glide.load.model.GlideUrl;
 
 class FastImageViewWithUrl extends ImageView {
     public GlideUrl glideUrl;
+    private OnDetachedFromWindowListener mOnDetachedFromWindowListener;
 
     public FastImageViewWithUrl(Context context) {
         super(context);
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (mOnDetachedFromWindowListener != null) {
+            mOnDetachedFromWindowListener.onDetached();
+        }
+    }
+
+    public void setOnDetachedFromWindowListener(OnDetachedFromWindowListener onDetachedFromWindowListener) {
+        mOnDetachedFromWindowListener = onDetachedFromWindowListener;
+    }
+
+    public interface OnDetachedFromWindowListener {
+        void onDetached();
     }
 }
